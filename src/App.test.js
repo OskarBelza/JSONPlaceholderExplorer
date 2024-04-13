81ffdb42af14d7132362ef9678cc9a8a583a64a6
grzegorz <grzeg@GRZEGORZ>
2024-04-13 20:14:08 +0200

    first unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header and controls', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Welcome to jsonplaceholder project/i);
+  const postLimitInput = screen.getByLabelText(/Post Limit:/i);
+  const minCharCountInput = screen.getByLabelText(/Min Char Count:/i);
+  const maxCharCountInput = screen.getByLabelText(/Max Char Count:/i);
+  const displayPostsButton = screen.getByText(/Display posts/i);
+  const displayCommentsButton = screen.getByText(/Display comments/i);
+  const displayAlbumsButton = screen.getByText(/Display albums/i);
+  const displayPhotosButton = screen.getByText(/Display photos/i);
+
+  expect(headerElement).toBeInTheDocument();
+  expect(postLimitInput).toBeInTheDocument();
+  expect(minCharCountInput).toBeInTheDocument();
+  expect(maxCharCountInput).toBeInTheDocument();
+  expect(displayPostsButton).toBeInTheDocument();
+  expect(displayCommentsButton).toBeInTheDocument();
+  expect(displayAlbumsButton).toBeInTheDocument();
+  expect(displayPhotosButton).toBeInTheDocument();
 });
